11a18231d20074d35604beead9ff69ef6432607e
kwakminsang <gwakminsang82@gmail.com>
2025-03-13 11:51:46 +0900

    to-do1

--- pr1.cpp
+++ pr1.cpp
@@ -1,17 +1,12 @@
-#include <csstudio>
-void main()
-{
-    int height;
+#include <iostream>
 
-    printf("5: ");
-    scanf("%d", &height);
-    for (int i=1; i<=height ; i++) {
-        for (int j=0 ; j<height-i ; j++)
-            printf("     ");
-        for (int j=0 ; j<i; j++)
-            printf("%3d", j*2+1);
-        for (int j=i-2; j>0; j--)
-            printf("%3d", j*2+1);
-         printf("\n");
+int main() {
+    
+    for (int i = 1; i <= 5; i++) 
+        for (int j = 1; j <= i; j++) {
+            std::cout << j; 
+        }
+        std::cout << std::endl; 
     }
-}      
+    return 0;
+}
